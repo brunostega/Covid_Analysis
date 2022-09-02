7daa9e93bf6790b68b9059f709200fe55bc424c1
brunostega <bruno.stega8@gmail.com>
2022-09-02 16:29:16 +0200

    covid commit 4

---
+++ README.md
@@ -0,0 +1,1 @@
+analysis of covid19 infection in Italy
---
+++ getData.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm *.csv
+wget https://github.com/pcm-dpc/COVID-19/blob/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv
